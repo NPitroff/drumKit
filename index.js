e8d1bf69470a9bc6966e4dd88a18c37519a0929a
NPitroff <nicolai.pitroff@gmail.com>
2022-02-01 06:42:21 -0800

    Added a drum audio variable and function to play sound in the loop

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 // selecting all the drum buttons
 const drumButtons = document.querySelectorAll(".drum")
+//selecting the audio file
+var drumAudio = new Audio("sounds/tom-1.mp3")
 // loop through each button to add an event listener
 drumButtons.forEach(drumButton => {
   drumButton.addEventListener('click', (e) =>{
-    console.log("Clicked!")
+    drumAudio.play();
   })
 })
