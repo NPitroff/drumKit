921fbe3656cbecfbde65b93f97fb5755358a57f3
NPitroff <nicolai.pitroff@gmail.com>
2022-02-01 07:38:05 -0800

    Button click to change the color to white

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
 // selecting all the drum buttons
-const drumButtons = document.querySelectorAll(".drum")
+const drumButtons = document.querySelectorAll(".drum").length
 //selecting the audio file
 var drumAudio = new Audio("sounds/tom-1.mp3")
 // loop through each button to add an event listener
-drumButtons.forEach(drumButton => {
-  drumButton.addEventListener('click', (e) =>{
-    drumAudio.play();
+for (var i=0; i < drumButtons; i++){
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    this.style.color = "white";
   })
-})
+}
+
+
+
+// drumAudio.play();
