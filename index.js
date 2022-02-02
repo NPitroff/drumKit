0dc7ddd9d039f0c56349947cfa23ee9a16f375bd
NPitroff <nicolai.pitroff@gmail.com>
2022-02-02 06:37:37 -0800

    Added a switch for all the kit buttons

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 
 // selecting all the drum buttons
 const drumButtons = document.querySelectorAll(".drum").length
-//selecting the audio file
-var drumAudio = new Audio("sounds/tom-1.mp3")
+
 // loop through each button to add an event listener
 for (var i=0; i < drumButtons; i++){
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    this.style.color = "white";
-  })
+    // variable to catch the button html
+    var buttonInnerHTML = this.innerHTML;
+    // switch statement to operate through the buttons
+    switch (buttonInnerHTML) {
+      // next button
+      case "w":
+      //selecting the audio file
+      var crashAudio = new Audio("sounds/crash.mp3")
+        crashAudio.play();
+        break;
+        // next button
+        case "a":
+        //selecting the audio file
+      var tomOneAudio = new Audio("sounds/tom-1.mp3")
+        tomOneAudio.play();
+        break;
+        // next button
+        case "s":
+        //selecting the audio file
+        var tomTwoAudio = new Audio("sounds/tom-2.mp3")
+        tomTwoAudio.play();
+        break;
+        // next button
+        case "d":
+        //selecting the audio file
+      var kickAudio = new Audio("sounds/kick-bass.mp3")
+        kickAudio.play();
+        break;
+        // next button
+        case "j":
+        //selecting the audio file
+        var snareAudio = new Audio("sounds/snare.mp3")
+        snareAudio.play();
+        break;
+        // next button
+        case "k":
+        //selecting the audio file
+      var tomThreeAudio = new Audio("sounds/tom-3.mp3")
+        tomThreeAudio.play();
+        break;
+        // next button
+        case "l":
+        //selecting the audio file
+      var tomFourAudio = new Audio("sounds/tom-4.mp3")
+        tomFourAudio.play();
+        break;
+      default:
+
+    }
+
+});
+
 }
-
-
 
 // drumAudio.play();
